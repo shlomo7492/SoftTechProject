a2d0858e5ba33573032b8153bd74f2053fe84a49
DimitarDaskalov <shlomo7492@yahoo.com>
2016-11-16 23:13:11 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mitko
+ * Date: 14.11.16
+ * Time: 13:19
+ */?>
+<html>
+<head>
+    <title>MyBlog::</title>
+</head>
+<body>
+<h1>Welcome to our new blog platform!</h1>
+<h3>Here are some $_GET variables printed:</h3>
+
+<?php
+echo "Parameter passed -> " . $_GET['id'].$_SERVER["QUERY_STRING"];
+?>
+Here is something...
+</br>
+<?php
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+?>
+
+</body>
+</html>
+
