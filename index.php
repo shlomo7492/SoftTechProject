faefe4ab76c0d23251e2882c352765c916318bc3
DimitarDaskalov <shlomo7492@yahoo.com>
2016-11-22 23:52:28 +0200

    Third Commit

--- index.php
+++ index.php
@@ -7,47 +7,20 @@
  * Time: 13:19
  */?>
 <?php
-    //Installing the blog
-    if ($_SESSION[INSTALL_PROCESS] == true) {
-        $myfile = fopen("config.inc", "w") or die("Unable to open file!");
+     //Installing the blog
+    include 'config.inc';
+    include 'install.inc';
 
-        $text = '$servername = "localhost"';
-        fwrite($myfile, $txt);
-
-
-        $text = '$username = "'.$_POST["user"].'";';
-        fwrite($myfile, $txt);
-
-        $text = '$password = "'.$_POST["password"].'";';
-        fwrite($myfile, $txt);
-
-        $text = '$dbname ="'.$_POST["dbname"].'";"';
-        fwrite($myfile, $txt);
-
-        fclose($myfile);
-    }
-    else { //Check if there is a valid DataBase without checking the DataBase itself.
-
-        include 'config.inc';
-        if ($dbname == "" || $username =="" || $password=="") {
-
-            $_SESSION["installed"] = false;
-            echo '<h2> Your blog Database needs to be configured!';
-            include '/actions/get_db_info.inc';
-        }
-        else {
-            include '/actions/setupDB.inc';
-        }
-    }
 ?>
-
+<p>Here comes our design...</p>
 <?php
+/*
     if ($site_location =='frontpage') {
         include '/template/front_main.tpl';
     }
     else{
         include '/template/front_main.tpl';
-    }
+    }*/
  ?>
 
 
