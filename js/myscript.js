199335cbeb5d36ef8217b62243da641c6c85f87d
DimitarDaskalov <shlomo7492@yahoo.com>
2016-12-20 11:42:25 +0200

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -7,8 +7,8 @@ function onResize() {
         document.getElementById("user_out").innerHTML = "Menu";
     }
     else{
-        document.getElementById("user_in").innerHTML ="<div class='menu'><ul ><li> <a href='../SoftTechProject'>Home</a>&nbsp;|&nbsp;</li> <li> <a href='blogs_view'>Blogs</a>&nbsp;|&nbsp;</li> <li>  <a href='contacts'>Feedback</a>&nbsp;|&nbsp;</li> <li><div class='dropdown'><button class='dropbtn'><a href='#'>Profile <span style='font-size:14px;'>&#9660;</span></a></button><div class='dropdown-content'><a href='?profile'>Profile info</a><a href='?myblog'>My Blog posts</a><a href='?blog/create'>Create Blog post</a><a href='?logout'>Logout</a></div></div></li> </ul> </div>";
-        document.getElementById("user_out").innerHTML ="<div class='menu'><ul ><li> <a href='../SoftTechProject'>Home</a>&nbsp;|&nbsp;</li> <li> <a href='blogs_view'>Blogs</a>&nbsp;|&nbsp;</li> <li>  <a href='register'>Register</a>&nbsp;|&nbsp;</li> <li>  <a href='login' class='login_link'>Login</a></li> </ul> </div>";
+        document.getElementById("user_in").innerHTML ="<div class='menu'><ul ><li> <a href='../SoftTechProject'>Home</a>&nbsp;|&nbsp;</li> <li> <a href='?blogs'>Blogs</a>&nbsp;|&nbsp;</li> <li>  <a href='?contacts'>Feedback</a>&nbsp;|&nbsp;</li> <li><div class='dropdown'><button class='dropbtn'><a href='#'>Profile <span style='font-size:14px;'>&#9660;</span></a></button><div class='dropdown-content'><a href='?profile'>Profile info</a><a href='?myarticles'>My articles</a><a href='?article/create'>Create new article</a><a href='?logout'>Logout</a></div></div></li> </ul> </div>";
+        document.getElementById("user_out").innerHTML ="<div class='menu'><ul ><li> <a href='../SoftTechProject'>Home</a>&nbsp;|&nbsp;</li> <li> <a href='?blogs'>Blogs</a>&nbsp;|&nbsp;</li> <li>  <a href='?register'>Register</a>&nbsp;|&nbsp;</li> <li>  <a href='?login' class='login_link'>Login</a></li> </ul> </div>";
 
     }
 }
